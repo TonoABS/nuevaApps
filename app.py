7e2b9035173b9dadc68dd6a591b11ec046a0260c
TonoABS <115069894+TonoABS@users.noreply.github.com>
2022-10-05 12:37:48 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+﻿"""
+[Aplicacion basica del microframework Flask de Python]
+Author: Fortinux
+Date: [05/10/2022]
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #return "<H1>Hola Mundo!</H1>"
+    return render_template("index.html")
+
+@app.route("/servicios")
+def servicios():
+    #return "<H1>Esta es la página de servicios</H1>"
+    return render_template("base.html")
+
+@app.route("/contacto")
+def contacto():
+    #return "<H1>Esta es la página de contacto</H1>"
+    return render_template("base.html")
+
+@app.route("/admin")
+def admin():
+    #return "<H1>Esta es la página de admin</H1>"
+    return render_template("base.html")
+
